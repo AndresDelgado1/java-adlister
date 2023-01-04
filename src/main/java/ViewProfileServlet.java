db1d7ed56f9ad09ab3a761c020c756906403f5d0
Andres Delgado <andresdelgado335@gmail.com>
2023-01-04 13:36:15 -0600

    finished cookie exercises

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,14 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+//        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+        if (request.getSession().getAttribute("user") != null) {
+            request.setAttribute("adminName", "Andres");
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+//            response.sendRedirect("/WEB-INF/profile.jsp");
+        } else{
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
 }
